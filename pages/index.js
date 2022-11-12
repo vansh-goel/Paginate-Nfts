6daadbf1df44ecb3a6d478da8af58942bb8f95b9
Vansh <62180044+vansh-goel@users.noreply.github.com>
2022-11-12 15:38:59 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { Metaplex } from '@metaplex-foundation/js';
 import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
+import { useEffect, useState } from 'react';
 
 const connection = new Connection(clusterApiUrl('mainnet-beta'));
 const mx = Metaplex.make(connection);
 
-const Home = () => {
+export default function Home() {
   const [address, setAddress] = useState(
     'Geh5Ss5knQGym81toYGXDbH3MFU2JCMK7E4QyeBHor1b',
   );
@@ -19,7 +19,15 @@ const Home = () => {
   const perPage = 1;
 
   const fetchNFTs = async () => {
-    // add some code here
+    try {
+      setLoading(true);
+      setCurrentView(null);
+      const list = await mx.nfts().findAllByOwner(new PublicKey(address));
+      setNftList(list);
+      setCurrentPage(1);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   useEffect(() => {
@@ -27,15 +35,34 @@ const Home = () => {
       return;
     }
 
-    // add some code here
+    const execute = async () => {
+      const startIndex = (currentPage - 1) * perPage;
+      const endIndex = currentPage * perPage;
+      await loadData(startIndex, endIndex);
+      setCurrentView(nftList.slice(startIndex, endIndex));
+      setLoading(false);
+    };
+    execute();
   }, [nftList, currentPage]);
 
   const loadData = async (startIndex, endIndex) => {
-    // add some code here
+    const nftsToLoad = nftList.filter((nft, index) => {
+      return (
+        index >= startIndex && index < endIndex && nft.metadataTask.isPending()
+      );
+    });
+
+    const promises = nftsToLoad.map((nft) => nft.metadataTask.run());
+    await Promise.all(promises);
   };
 
   const changeCurrentPage = (operation) => {
-    // add some code here
+    setLoading(true);
+    if (operation === 'next') {
+      setCurrentPage((prevValue) => prevValue + 1);
+    } else {
+      setCurrentPage((prevValue) => (prevValue > 1 ? prevValue - 1 : 1));
+    }
   };
 
   return (
@@ -83,9 +110,7 @@ const Home = () => {
                 Prev Page
               </button>
               <button
-                disabled={
-                  nftList && Math.ceil(nftList.length / perPage) === currentPage
-                }
+                disabled={nftList && Math.ceil(nftList.length / perPage) === currentPage}
                 className={styles.styledButton}
                 onClick={() => changeCurrentPage('next')}
               >
@@ -97,6 +122,4 @@ const Home = () => {
       </div>
     </div>
   );
-};
-
-export default Home;
+}
